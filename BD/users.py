fe06e7d55705624c7c4c77a4826c8517bf03d316
Anis Nashef <anisnashefsch@gmail.com>
2026-06-15 17:19:42 +0200

    gerer quelques erreurs possible

--- BD/users.py
+++ BD/users.py
@@ -24,6 +24,12 @@ def create_users_table():
     conn.close()
 
 def creer_utilisateur(username, email, mot_de_passe):
+    #precheck si l'email ou le username existe déjà
+    if check_user_exists(email):
+        return "email_pris"
+    if username_exists(username):
+        return "username_pris"
+    
     # générer un sel aléatoire unique
     sel = os.urandom(16).hex()          # 16 octets aléatoires, en texte hexa
     # hasher le mot de passe
@@ -36,12 +42,20 @@ def creer_utilisateur(username, email, mot_de_passe):
     # insérer en base
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO users (username, email, mdp_hash, sel) VALUES (?, ?, ?, ?)",
-        (username, email, mdp_hash, sel)
-    )
-    conn.commit()
-    conn.close()
+    try:
+        cursor.execute(
+            "INSERT INTO users (username, email, mdp_hash, sel) VALUES (?, ?, ?, ?)",
+            (username, email, mdp_hash, sel)
+        )
+        conn.commit()
+    except sqlite3.IntegrityError as e:
+        if "email" in str(e):
+            return "email_pris"
+        elif "username" in str(e):
+            return "username_pris"
+        return None
+    finally:
+        conn.close()
 
 def verifier_identifiants(username ,email, mot_de_passe):
     conn = sqlite3.connect(DB_PATH)
@@ -98,3 +112,18 @@ def delete_user(email):
     conn.commit()
     conn.close()
 
+def check_user_exists(email):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1 FROM users WHERE email = ?", (email,))
+    exists = cursor.fetchone() is not None
+    conn.close()
+    return exists
+
+def username_exists(username):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1 FROM users WHERE username = ?", (username,))
+    existe = cursor.fetchone() is not None
+    conn.close()
+    return existe
